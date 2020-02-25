cabacfb1d924e018dcbdda270e141480ae58725a
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2020-02-25 22:19:34 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let str = Math.abs(parseInt(n)).toString();
   
+  let arr = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+      arr.push(str[i]);
+  }
+
+  let result = arr.join('');
+  return parseInt(result.toString());
 }
